a1748610df54cb3c561de9853111c5142c532cc1
Shorokhov Klim <admin@u22.isp>
2023-10-21 16:54:56 +0300

    feat: start db in docker and server

--- config/settings.py
+++ config/settings.py
@@ -201,11 +201,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+# static
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+# media
+STATIC_ROOT = BASE_DIR / "django_static"
+MEDIA_ROOT = BASE_DIR / "django_media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = "/media/"
